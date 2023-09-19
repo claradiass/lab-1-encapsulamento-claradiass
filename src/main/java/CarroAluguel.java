90be0514d379dec936fb7086c37ed0cc6c52b37f
claradiass <claragoncalves370@gmail.com>
2023-09-18 21:59:40 -0300

    Ultima alteração

--- src/main/java/CarroAluguel.java
+++ src/main/java/CarroAluguel.java
@@ -7,6 +7,9 @@ public class CarroAluguel {
 
 
 
+
+
+
     public CarroAluguel(float valorPorKm){
         this.valorPorKm = valorPorKm;
         this.distancia = 0;
@@ -15,29 +18,37 @@ public class CarroAluguel {
         this.debito = 0;
     }
 
+
     public float getValorPorKm() {
         return valorPorKm;
     }
+
 
     public void setValorPorKm(float valorPorKm) {
         this.valorPorKm = valorPorKm;
     }
 
-    
+
+   
+
 
     public void setDistanciaPercorrida(int distancia) {
         this.distancia= distancia;
     }
 
+
     public float getDistanciaPercorrida() {
         return distancia;
     }
+
 
     public boolean isDisponivel(){
         return disponivel;
     }
 
-    
+
+   
+
 
     public void alugar() throws CarroIndisponivelException{
         if(disponivel){
@@ -47,41 +58,49 @@ public class CarroAluguel {
             throw new CarroIndisponivelException("O carro está indisponível.");
         }
 
+
     }
 
-    public void devolver() throws CarroDisponivelException, CarroNaoPagoException{
+
+    public void devolver() throws CarroDisponivelException, CarroNaoPagoException {
         if (disponivel) {
             throw new CarroDisponivelException("O carro está disponível.");
         }
     
-        if (debito == 0) {
-            disponivel = true;
-            sinistro = false;
-            distancia = 0;
-        } else {
+        if (distancia > 0 && debito == 0) {
             throw new CarroNaoPagoException("O carro não foi pago.");
         }
+    
+        disponivel = true;
+        sinistro = false;
+        distancia = 0;
     }
+
 
     public boolean hasSinistro(){
         return sinistro;
     }
 
+
     public void setSinistro(boolean sinistro) {
         this.sinistro = sinistro;
     }
 
+
     public float getDebito(){
         float debitoAtual = valorPorKm * distancia;
 
+
         if (sinistro) {
-            float percentual = debitoAtual * 0.6f; 
+            float percentual = debitoAtual * 0.6f;
             debitoAtual += percentual;
             this.sinistro = false;
         }
 
+
         return debitoAtual;
     }
+
 
     public void pagar() throws CarroDisponivelException{
         if(!disponivel){
@@ -92,23 +111,5 @@ public class CarroAluguel {
         }
     }
 
-    
-
-    
-
-
 
 }
-
-
-
-    
-
-
-
-
-
-
-
-
-
